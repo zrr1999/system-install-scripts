aaccb049660baf6cf3a5f58e91428ba243c09181
zrr1999 <2742392377@qq.com>
2024-09-06 19:50:15 +0800

    update root-gateway

--- root-gateway/usr/share/mihomo/update.sh
+++ root-gateway/usr/share/mihomo/update.sh
@@ -2,4 +2,3 @@
 
 curl -o /etc/mihomo/original.yaml -A "clash-verge/1.7.5" $1
 bash /usr/share/mihomo/merge.sh
-systemctl restart mihomo
--- root-gateway/usr/share/mosdns/update.sh
+++ root-gateway/usr/share/mosdns/update.sh
@@ -4,12 +4,10 @@ mosdns_data_dir="/var/mosdns/"
 mkdir $mosdns_data_dir
 echo 'update start'
 cd $(mktemp -d)
-curl https://fastly.jsdelivr.net/gh/pmkol/easymosdns@rules/china_domain_list.txt > ./china_domain_list.txt
-curl https://fastly.jsdelivr.net/gh/pmkol/easymosdns@rules/cdn_domain_list.txt > ./cdn_domain_list.txt 
-curl https://fastly.jsdelivr.net/gh/pmkol/easymosdns@rules/gfw_domain_list.txt > ./gfw_domain_list.txt
-curl https://fastly.jsdelivr.net/gh/pmkol/easymosdns@rules/ad_domain_list.txt > ./ad_domain_list.txt
-curl https://fastly.jsdelivr.net/gh/pmkol/easymosdns@rules/china_ip_list.txt > ./china_ip_list.txt
-curl https://fastly.jsdelivr.net/gh/pmkol/easymosdns@rules/gfw_ip_list.txt > ./gfw_ip_list.txt
+curl https://fastly.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/direct-list.txt > ./direct-list.txt
+curl https://fastly.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/proxy-list.txt > ./proxy-list.txt
+curl https://fastly.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/reject-list.txt > ./reject-list.txt
+curl https://fastly.jsdelivr.net/gh/Loyalsoldier/geoip@release/text/cn.txt > ./cn.txt
 \cp -rf ./*.txt $mosdns_data_dir
 rm -rf ./* 
 echo 'update successful'
